42674390f45710dbd4a2f71bf158f90ffaca2f14
llt3055 <llt30555@gmail.com>
2026-02-15 05:48:17 +0800

    add one more prototype and content of  enterArrayData function

--- lab6.cpp
+++ lab6.cpp
@@ -8,4 +8,16 @@ const int SIZE = 5;
 
 //FUNCTION PROTOTYPE
 void enterArrayData(double*, int);
-void outputArrayData(double*, int)
+void outputArrayData(double*, int);
+double sumArray(double*);
+
+int main(){
+
+}
+
+void enterArrayData(double *arr, int size){
+    cout << "Data entry for the array:" << endl;
+    for (int i =0; i< size; i++){
+        
+    }
+}
