044101c686669daa2385d7d414af90a670f392fe
llt3055 <llt30555@gmail.com>
2026-02-15 05:34:12 +0800

    constant setup and function prototype

--- lab6.cpp
+++ lab6.cpp
@@ -1,1 +1,11 @@
 // COMSC-210 | Lab  6| Tianyi Cao
+#include <iostream>
+
+using namespace std;
+
+//CREATE THE SIZE FOR ARRAY AVOIDING MAGIC NUMBER
+const int SIZE = 5;
+
+//FUNCTION PROTOTYPE
+void enterArrayData(double*, int);
+void outputArrayData(double*, int)
